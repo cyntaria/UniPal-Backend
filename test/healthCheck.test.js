a5d78bf8557d5be225cdf1e823d01bfbc0a9864b
arafaysaleem <a.rafaysaleem@gmail.com>
2021-08-26 15:39:14 -0700

    refact(Test): use chai.expect for healthCheck test

--- test/healthCheck.test.js
+++ test/healthCheck.test.js
@@ -1,29 +1,27 @@
 /* eslint-disable no-undef */
 const request = require("supertest");
+const expect = require('chai').expect;
 const app = require('../src/server');
 
 describe("Healthcheck", () => {
-    it("returns OK, success and up, if server is healthy", (done) => {
-        request(app).get("/api/v1/health", null)
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(function(res) {
-                const error = res.body.headers.error;
-                const uptime = res.body.body.uptime;
-                const health = res.body.body.health;
-                res.body = {};
-                res.body.error = error;
-                res.body.up = uptime > 0;
-                res.body.health = health;
-            })
-            .expect(200, {
-                health: 'OK',
-                error: 0,
-                up: true
-            })
-            .end(function(err) {
-                if (err) return done(err);
-                return done();
-            });
+    it("returns OK, uptime and timestamp, if server is healthy", async() => {
+        const res = await request(app).get("/api/v1/health", null);
+
+        // status check
+        expect(res.status).to.be.equal(200);
+
+        // error check
+        expect(res.body.headers.error).to.be.equal(0);
+        
+        // params check
+        const resBody = res.body.body;
+        expect(resBody).to.include.keys("health", "uptime", "timestamp");
+
+        // health check
+        expect(resBody.health).to.be.equal('OK');
+
+        // uptime check
+        const uptime = Number.parseFloat(resBody.uptime);
+        expect(uptime).to.be.gt(0);
     });
 });
