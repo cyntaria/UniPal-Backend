66d092f8e84d20a5499915c1dfe193f17f6a2b01
arafaysaleem <a.rafaysaleem@gmail.com>
2021-08-26 14:27:29 -0700

    refact(Model): fix incorrect field name

--- src/models/student.model.js
+++ src/models/student.model.js
@@ -33,19 +33,19 @@ class StudentModel {
     // 24 fields
     create = async({ erp, first_name, last_name, gender, contact, email, birthday, password,
         profile_picture_url, graduation_year, uni_email, hobby_1, hobby_2, hobby_3,
-        interest_1, interest_2, interest_3, program_id, campus_id, current_user_status,
+        interest_1, interest_2, interest_3, program_id, campus_id, current_status,
         favourite_campus_hangout_spot, favourite_campus_activity, is_active = '1', role = Roles.ApiUser }) => {
         const sql = `INSERT INTO ${tables.Students}
         (erp, first_name, last_name, gender, contact, email, birthday, password,
             profile_picture_url, graduation_year, uni_email, hobby_1, hobby_2, hobby_3,
-            interest_1, interest_2, interest_3, program_id, campus_id, current_user_status,
+            interest_1, interest_2, interest_3, program_id, campus_id, current_status,
             favourite_campus_hangout_spot, favourite_campus_activity, is_active, role)
             VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
 
         const result = await DBService.query(sql, [erp, first_name, last_name, gender,
             contact, email, birthday, password, profile_picture_url, graduation_year,
             uni_email, hobby_1, hobby_2, hobby_3, interest_1, interest_2, interest_3,
-            program_id, campus_id, current_user_status,
+            program_id, campus_id, current_status,
             favourite_campus_hangout_spot, favourite_campus_activity, is_active, role]);
         const created_student = !result ? 0 : {
             erp: erp,
