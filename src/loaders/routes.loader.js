04a376a8943802a612efe23162d8ab33435bef5b
arafaysaleem <a.rafaysaleem@gmail.com>
2021-09-01 02:53:32 -0700

    add(Routes): add hobby routes

--- src/loaders/routes.loader.js
+++ src/loaders/routes.loader.js
@@ -1,12 +1,14 @@
 /* Routes */
 const authRouter = require('../routes/auth.routes');
 const studentRouter = require('../routes/student.routes');
+const hobbyRouter = require('../routes/hobby.routes');
 const healthCheckRouter = require('../routes/healthCheck.routes');
 
 class RoutesLoader {
     static initRoutes(app, version) {
         app.use(`/api/${version}/auth`, authRouter);
         app.use(`/api/${version}/students`, studentRouter);
+        app.use(`/api/${version}/hobbies`, hobbyRouter);
         app.use(`/api/${version}/health`, healthCheckRouter);
     }
 }
---
+++ src/routes/hobby.routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth.middleware');
+const awaitHandlerFactory = require('../middleware/awaitHandlerFactory.middleware');
+
+const hobbyController = require('../controllers/hobby.controller');
+const { Roles } = require('../utils/enums/roles.utils');
+const { createHobbySchema, updateHobbySchema } = require('../middleware/validators/hobbyValidator.middleware');
+
+router.get('/', auth(), awaitHandlerFactory(hobbyController.getAllHobbies)); // localhost:3000/api/v1/hobbies
+router.get('/:id', auth(), awaitHandlerFactory(hobbyController.getHobbyById)); // localhost:3000/api/v1/hobbies/1
+router.post('/', auth(Roles.Admin), createHobbySchema, awaitHandlerFactory(hobbyController.createHobby)); // localhost:3000/api/v1/hobbies
+router.patch('/:id', auth(Roles.Admin), updateHobbySchema, awaitHandlerFactory(hobbyController.updateHobby)); // localhost:3000/api/v1/hobbies/1 , using patch for partial update
+router.delete('/:id', auth(Roles.Admin), awaitHandlerFactory(hobbyController.deleteHobby)); // localhost:3000/api/v1/hobbies/1
+
+module.exports = router;
