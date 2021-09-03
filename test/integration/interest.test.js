daa4245d08a508841225f04bc75eb75b27e3b1a7
arafaysaleem <a.rafaysaleem@gmail.com>
2021-09-03 08:02:32 -0700

    tests(Interests-API): add integration tests

---
+++ test/integration/interest.test.js
@@ -0,0 +1,400 @@
+/* eslint-disable no-undef */
+const request = require("supertest");
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const decache = require('decache');
+const jwt = require('jsonwebtoken');
+const {Config} = require('../../src/configs/config');
+
+describe("Interests API", () => {
+    const API = "/api/v1/interests";
+    const adminERP = '15030';
+    const userERP = '17855';
+    const existingInterest = {
+        interest_id: 1,
+        interest: 'sports'
+    };
+    const userToken = jwt.sign({erp: userERP}, Config.SECRET_JWT); // non expiry token
+    const adminToken = jwt.sign({erp: adminERP}, Config.SECRET_JWT);
+
+    beforeEach(() => {
+        this.app = require('../../src/server').setup();
+    });
+
+    context("GET /interests", () => {
+
+        it("Scenario 1: Get all interests request successful", async() => {
+            // act
+            let res = await request(this.app)
+                .get(`${API}`)
+                .auth(userToken, { type: 'bearer' });
+    
+            // assert
+            expect(res.status).to.be.equal(200);
+            expect(res.body.headers.error).to.be.equal(0);
+            const resBody = res.body.body;
+            expect(resBody).to.be.an('array');
+            expect(resBody[0]).to.include.keys(['interest_id', 'interest']); // deep compare two objects using 'eql'
+        });
+
+        it("Scenario 2: Get all interests request unsuccessful", async() => {
+            // arrange
+            decache('../../src/server');
+            const InterestModel = require('../../src/models/interest.model');
+            const modelStub = sinon.stub(InterestModel, 'findAll').callsFake(() => []); // return empty interest list
+            const app = require('../../src/server').setup();
+
+            // act
+            const res = await request(app)
+                .get(`${API}`)
+                .auth(userToken, { type: 'bearer' });
+    
+            // assert
+            expect(res.status).to.be.equal(404);
+            expect(res.body.headers.error).to.be.equal(1);
+            expect(res.body.headers.code).to.be.equal('NotFoundException');
+            expect(res.body.headers.message).to.be.equal('Interests not found');
+            modelStub.restore();
+        });
+
+        it("Scenario 3: Get all interests request is unauthorized", async() => {
+            // act
+            let res = await request(this.app).get(`${API}`);
+    
+            // assert
+            expect(res.status).to.be.equal(401);
+            expect(res.body.headers.error).to.be.equal(1);
+            expect(res.body.headers.code).to.be.equal('TokenMissingException');
+            expect(res.body.headers.message).to.be.equal('Access denied. No token credentials sent');
+        });
+    });
+
+    context("GET /interests/:id", () => {
+        it("Scenario 1: Get a interest request successful", async() => {
+            // act
+            let res = await request(this.app)
+                .get(`${API}/${existingInterest.interest_id}`)
+                .auth(userToken, { type: 'bearer' });
+
+            // assert
+            expect(res.status).to.be.equal(200);
+            expect(res.body.headers.error).to.be.equal(0);
+            const resBody = res.body.body;
+            expect(resBody).to.include.keys(['interest_id', 'interest']);
+            expect(resBody.interest_id).to.be.eql(existingInterest.interest_id); // should match initially sent id
+        });
+
+        it("Scenario 2: Get a interest request is unsuccessful", async() => {
+            // arrange
+            const unknownInterestId = 2000;
+
+            // act
+            const res = await request(this.app)
+                .get(`${API}/${unknownInterestId}`)
+                .auth(userToken, { type: 'bearer' });
+    
+            // assert
+            expect(res.status).to.be.equal(404);
+            expect(res.body.headers.error).to.be.equal(1);
+            expect(res.body.headers.code).to.be.equal('NotFoundException');
+            expect(res.body.headers.message).to.be.equal('Interest not found');
+        });
+
+        it("Scenario 3: Get a interest request is unauthorized", async() => {
+            // act
+            let res = await request(this.app).get(`${API}/${existingInterest.interest_id}`);
+    
+            // assert
+            expect(res.status).to.be.equal(401);
+            expect(res.body.headers.error).to.be.equal(1);
+            expect(res.body.headers.code).to.be.equal('TokenMissingException');
+            expect(res.body.headers.message).to.be.equal('Access denied. No token credentials sent');
+        });
+    });
+
+    context("POST /interests", () => {
+        const interest = 'gaming';
+        
+        it("Scenario 1: Create a interest request is successful", async() => {
+            // arrange
+            const data = { interest };
+            const app = this.app;
+
+            // act
+            let res = await request(app)
+                .post(`${API}`)
+                .auth(adminToken, { type: 'bearer' })
+                .send(data);
+    
+            // assert
+            expect(res.status).to.be.equal(201);
+            expect(res.body.headers.error).to.be.equal(0);
+            expect(res.body.body).to.include.keys(['interest_id', 'affected_rows']);
+            expect(res.body.body.affected_rows).to.be.equal(1);
+            const newId = res.body.body.interest_id;
+
+            // affirm
+            res = await request(app)
+                .get(`${API}/${newId}`)
+                .auth(userToken, { type: 'bearer' });
+
+            expect(res.status).to.be.equal(200);
+            expect(res.body.body).to.be.eql({
+                interest_id: newId,
+                interest: interest
+            });
+
+            // cleanup
+            res = await request(app)
+                .delete(`${API}/${newId}`)
+                .auth(adminToken, { type: 'bearer' });
+            expect(res.status).to.be.equal(200);
+        });
+
+        it("Scenario 2: Create a interest request is incorrect", async() => {
+            // arrange
+            const data = {
+                interests: interest // <-- a valid parameter name should be 'interest'
+            };
+
+            // act
+            const res = await request(this.app)
+                .post(`${API}`)
+                .auth(adminToken, { type: 'bearer' })
+                .send(data);
+    
+            // assert
+            expect(res.status).to.be.equal(422);
+            expect(res.body.headers.error).to.be.equal(1);
+            expect(res.body.headers.code).to.be.equal('InvalidPropertiesException');
+            const incorrectParams = res.body.headers.data.map(o => (o.param));
+            expect(incorrectParams).to.include('interest');
+        });
+
+        it("Scenario 3: Create a interest request is forbidden", async() => {
+            // arrange
+            const data = { interest };
+
+            // act
+            const res = await request(this.app)
+                .post(`${API}`)
+                .auth(userToken, { type: 'bearer' }) // <-- api_user token instead of admin token
+                .send(data);
+            
+            // assert
+            expect(res.status).to.be.equal(403);
+            expect(res.body.headers.error).to.be.equal(1);
+            expect(res.body.headers.code).to.be.equal('ForbiddenException');
+            expect(res.body.headers.message).to.be.equal('User unauthorized for action');
+        });
+
+        it("Scenario 4: Create a interest request is unauthorized", async() => {
+            // arrange
+            const data = { interest };
+
+            // act
+            const res = await request(this.app)
+                .post(`${API}`)
+                .send(data);
+    
+            // assert
+            expect(res.status).to.be.equal(401);
+            expect(res.body.headers.error).to.be.equal(1);
+            expect(res.body.headers.code).to.be.equal('TokenMissingException');
+            expect(res.body.headers.message).to.be.equal('Access denied. No token credentials sent');
+        });
+    });
+
+    context("PATCH /interests/:id", () => {
+        const newInterest = 'gaming';
+        
+        it("Scenario 1: Update a interest request is successful", async() => {
+            // arrange
+            const data = { interest: newInterest };
+            const app = this.app;
+
+            // act
+            let res = await request(app)
+                .patch(`${API}/${existingInterest.interest_id}`)
+                .auth(adminToken, { type: 'bearer' })
+                .send(data);
+    
+            // assert
+            expect(res.status).to.be.equal(200);
+            expect(res.body.headers.error).to.be.equal(0);
+            expect(res.body.body.rows_matched).to.be.equal(1);
+            expect(res.body.body.rows_changed).to.be.equal(1);
+
+            
+            // affirm
+            res = await request(app)
+                .get(`${API}/${existingInterest.interest_id}`)
+                .auth(userToken, { type: 'bearer' });
+            
+            expect(res.status).to.be.equal(200);
+            expect(res.body.body).to.be.eql({
+                interest_id: existingInterest.interest_id,
+                interest: newInterest
+            });
+            
+            // cleanup
+            data.interest = existingInterest.interest;
+            res = await request(app)
+                .patch(`${API}/${existingInterest.interest_id}`)
+                .auth(adminToken, { type: 'bearer' })
+                .send(data);
+            expect(res.status).to.be.equal(200);
+        });
+
+        it("Scenario 2: Update a interest request is unsuccessful", async() => {
+            // arrange
+            const data = { interest: newInterest };
+            const unknownInterestId = 2000;
+
+            // act
+            const res = await request(this.app)
+                .patch(`${API}/${unknownInterestId}`)
+                .auth(adminToken, { type: 'bearer' })
+                .send(data);
+    
+            // assert
+            expect(res.status).to.be.equal(404);
+            expect(res.body.headers.error).to.be.equal(1);
+            expect(res.body.headers.code).to.be.equal('NotFoundException');
+            expect(res.body.headers.message).to.be.equal('Interest not found');
+        });
+
+        it("Scenario 3: Update a interest request is incorrect", async() => {
+            // arrange
+            const data = {
+                interests: newInterest // <-- a valid parameter name should be 'interest'
+            };
+
+            // act
+            const res = await request(this.app)
+                .patch(`${API}/${existingInterest.interest_id}`)
+                .auth(adminToken, { type: 'bearer' })
+                .send(data);
+    
+            // assert
+            expect(res.status).to.be.equal(422);
+            expect(res.body.headers.error).to.be.equal(1);
+            expect(res.body.headers.code).to.be.equal('InvalidPropertiesException');
+            const incorrectParams = res.body.headers.data.map(o => (o.param));
+            expect(incorrectParams).to.include('interest');
+        });
+
+        it("Scenario 3: Update a interest request is forbidden", async() => {
+            // arrange
+            const data = { interest: newInterest };
+
+            // act
+            const res = await request(this.app)
+                .patch(`${API}/${existingInterest.interest_id}`)
+                .auth(userToken, { type: 'bearer' }) // <-- api_user token instead of admin token
+                .send(data);
+            
+            // assert
+            expect(res.status).to.be.equal(403);
+            expect(res.body.headers.error).to.be.equal(1);
+            expect(res.body.headers.code).to.be.equal('ForbiddenException');
+            expect(res.body.headers.message).to.be.equal('User unauthorized for action');
+        });
+
+        it("Scenario 4: Update a interest request is unauthorized", async() => {
+            // arrange
+            const data = { interest: newInterest };
+
+            // act
+            const res = await request(this.app)
+                .patch(`${API}/${existingInterest.interest_id}`)
+                .send(data);
+    
+            // assert
+            expect(res.status).to.be.equal(401);
+            expect(res.body.headers.error).to.be.equal(1);
+            expect(res.body.headers.code).to.be.equal('TokenMissingException');
+            expect(res.body.headers.message).to.be.equal('Access denied. No token credentials sent');
+        });
+    });
+
+    context("DELETE /interests", () => {
+        const interest = 'gaming';
+        
+        it("Scenario 1: Delete a interest request is successful", async() => {
+            // prepare
+            const data = { interest };
+            const app = this.app;
+
+            // create dummy
+            let res = await request(app)
+                .post(`${API}`)
+                .auth(adminToken, { type: 'bearer' })
+                .send(data);
+            expect(res.status).to.be.equal(201);
+
+            // arrange
+            const newId = res.body.body.interest_id;
+
+            // act
+            res = await request(app)
+                .delete(`${API}/${newId}`)
+                .auth(adminToken, { type: 'bearer' });
+
+            // assert
+            expect(res.status).to.be.equal(200);
+            expect(res.body.headers.error).to.be.equal(0);
+            expect(res.body.headers.message).to.be.equal('Interest has been deleted');
+
+            // affirm
+            res = await request(app)
+                .get(`${API}/${newId}`)
+                .auth(userToken, { type: 'bearer' });
+            expect(res.status).to.be.equal(404);
+            expect(res.body.headers.error).to.be.equal(1);
+            expect(res.body.headers.code).to.be.equal('NotFoundException');
+        });
+
+        it("Scenario 2: Delete a interest request is unsuccessful", async() => {
+            // arrange
+            const unknownInterestId = 2000;
+
+            // act
+            const res = await request(this.app)
+                .delete(`${API}/${unknownInterestId}`)
+                .auth(adminToken, { type: 'bearer' });
+    
+            // assert
+            expect(res.status).to.be.equal(404);
+            expect(res.body.headers.error).to.be.equal(1);
+            expect(res.body.headers.code).to.be.equal('NotFoundException');
+            expect(res.body.headers.message).to.be.equal('Interest not found');
+        });
+
+        it("Scenario 3: Delete a interest request is forbidden", async() => {
+            // act
+            const res = await request(this.app)
+                .delete(`${API}/${existingInterest.interest_id}`)
+                .auth(userToken, { type: 'bearer' }); // <-- api_user token instead of admin token
+            
+            // assert
+            expect(res.status).to.be.equal(403);
+            expect(res.body.headers.error).to.be.equal(1);
+            expect(res.body.headers.code).to.be.equal('ForbiddenException');
+            expect(res.body.headers.message).to.be.equal('User unauthorized for action');
+        });
+
+        it("Scenario 4: Delete a interest request is unauthorized", async() => {
+            // act
+            const res = await request(this.app)
+                .delete(`${API}/${existingInterest.interest_id}`);
+    
+            // assert
+            expect(res.status).to.be.equal(401);
+            expect(res.body.headers.error).to.be.equal(1);
+            expect(res.body.headers.code).to.be.equal('TokenMissingException');
+            expect(res.body.headers.message).to.be.equal('Access denied. No token credentials sent');
+        });
+    });
+
+});
