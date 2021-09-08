3a5ce42d8fc87a3d1b643192102d1540b47d2ec4
arafaysaleem <a.rafaysaleem@gmail.com>
2021-09-08 14:06:23 -0700

    add(Auth-Middleware): add a resource owner check

--- src/routes/student.routes.js
+++ src/routes/student.routes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { auth } = require('../middleware/auth.middleware');
+const { auth, ownerAuth } = require('../middleware/auth.middleware');
 const awaitHandlerFactory = require('../middleware/awaitHandlerFactory.middleware');
 
 const studentController = require('../controllers/student.controller');
 const { Roles } = require('../utils/enums/roles.utils');
-const { updateStudentSchema, getStudentsQuerySchema: getStudentQuerySchema } = require('../middleware/validators/studentValidator.middleware');
+const { updateStudentSchema, getStudentsQuerySchema } = require('../middleware/validators/studentValidator.middleware');
 
-router.get('/', auth(), getStudentQuerySchema, awaitHandlerFactory(studentController.getAllStudents)); // localhost:3000/api/v1/students
-router.get('/:erp', auth(), getStudentQuerySchema, awaitHandlerFactory(studentController.getStudentById)); // localhost:3000/api/v1/students/1
-router.patch('/:erp', auth(Roles.Admin), updateStudentSchema, awaitHandlerFactory(studentController.updateStudent)); // localhost:3000/api/v1/students/1
+router.get('/', auth(), getStudentsQuerySchema, awaitHandlerFactory(studentController.getAllStudents)); // localhost:3000/api/v1/students
+router.get('/:erp', auth(), getStudentsQuerySchema, awaitHandlerFactory(studentController.getStudentById)); // localhost:3000/api/v1/students/1
+router.patch('/:erp', auth(Roles.Admin, Roles.ApiUser), ownerAuth(Roles.ApiUser), updateStudentSchema, awaitHandlerFactory(studentController.updateStudent)); // localhost:3000/api/v1/students/1
 router.delete('/:erp', auth(Roles.Admin), awaitHandlerFactory(studentController.deleteStudent)); // localhost:3000/api/v1/students/1
 
 module.exports = router;
