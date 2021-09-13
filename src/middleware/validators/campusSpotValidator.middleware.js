3a8cd724f364fabfbd412fffb49682776a9498a5
arafaysaleem <a.rafaysaleem@gmail.com>
2021-09-13 01:01:47 -0700

    add(CampusSpots-API): add endpoint implementations

---
+++ src/middleware/validators/campusSpotValidator.middleware.js
@@ -0,0 +1,36 @@
+const { body } = require('express-validator');
+
+exports.createCampusSpotSchema = [
+    body('campus_spot')
+        .trim()
+        .exists()
+        .withMessage('Campus spot is required')
+        .isLength({min: 3})
+        .withMessage('Campus spot should be atleast 3 letters'),
+    body('campus_id')
+        .trim()
+        .exists()
+        .withMessage('Campus id is required for the spot')
+        .isInt({ min: 1 })
+        .withMessage('Invalid CampusID found')
+];
+
+exports.updateCampusSpotSchema = [
+    body('campus_spot')
+        .exists()
+        .withMessage('New campus spot is required')
+        .exists()
+        .isLength({min: 3})
+        .withMessage('Campus spot should be atleast 3 letters'),
+    body()
+        .custom(value => {
+            return Object.keys(value).length !== 0;
+        })
+        .withMessage('Please provide required fields to update')
+        .custom(value => {
+            const updates = Object.keys(value);
+            const allowUpdates = ["campus_spot"];
+            return updates.every(update => allowUpdates.includes(update));
+        })
+        .withMessage('Invalid updates!')
+];
---
+++ src/models/campusSpot.model.js
@@ -0,0 +1,66 @@
+const { DBService } = require('../db/db-service');
+const { multipleColumnSet, multipleFilterSet } = require('../utils/common.utils');
+const { tables } = require('../utils/tableNames.utils');
+
+class CampusSpotModel {
+
+    findAll = async(filters) => {
+        let sql = `SELECT * FROM ${tables.CampusSpots}`;
+
+        if (!Object.keys(filters).length) {
+            return await DBService.query(sql);
+        }
+
+        const { filterSet, filterValues } = multipleFilterSet(filters);
+        sql += ` WHERE ${filterSet}`;
+
+        return await DBService.query(sql, [...filterValues]);
+    }
+
+    findOne = async(filters) => {
+        const { filterSet, filterValues } = multipleFilterSet(filters);
+
+        const sql = `SELECT * FROM ${tables.CampusSpots}
+        WHERE ${filterSet}`;
+
+        const result = await DBService.query(sql, [...filterValues]);
+
+        return result[0];
+    }
+
+    create = async({ campus_spot, campus_id }) => {
+        const valueSet = { campus_spot, campus_id };
+        const { columnSet, values } = multipleColumnSet(valueSet);
+
+        const sql = `INSERT INTO ${tables.CampusSpots} SET ${columnSet}`;
+
+        const result = await DBService.query(sql, [...values]);
+        const created_campus_spot = !result ? 0 : {
+            campus_spot_id: result.insertId,
+            affected_rows: result.affectedRows
+        };
+
+        return created_campus_spot;
+    }
+
+    update = async(columns, id) => {
+        const { columnSet, values } = multipleColumnSet(columns);
+
+        const sql = `UPDATE ${tables.CampusSpots} SET ${columnSet} WHERE campus_spot_id = ?`;
+
+        const result = await DBService.query(sql, [...values, id]);
+
+        return result;
+    }
+
+    delete = async(id) => {
+        const sql = `DELETE FROM ${tables.CampusSpots}
+        WHERE campus_spot_id = ?`;
+        const result = await DBService.query(sql, [id]);
+        const affectedRows = result ? result.affectedRows : 0;
+
+        return affectedRows;
+    }
+}
+
+module.exports = new CampusSpotModel;
