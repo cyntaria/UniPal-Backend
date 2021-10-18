1f176a223c71a3a8c990feca6b20afba3afc005c
arafaysaleem <a.rafaysaleem@gmail.com>
2021-10-18 00:53:56 -0700

    add(Models): create post reaction and upload model

---
+++ src/models/postUpload.model.js
@@ -0,0 +1,45 @@
+const { DBService } = require('../db/db-service');
+const { multipleColumnSet } = require('../utils/common.utils');
+const { tables } = require('../utils/tableNames.utils');
+
+class PostUploadModel {
+
+    create = async({ post_id, resource_url, resource_type }) => {
+        const valueSet = { post_id, resource_url, resource_type };
+        const { columnSet, values } = multipleColumnSet(valueSet);
+
+        const sql = `INSERT INTO ${tables.PostUploads} SET ${columnSet}`;
+
+        const result = await DBService.query(sql, [...values]);
+        const created_resource = !result ? 0 : {
+            resource_id: result.insertId,
+            affected_rows: result.affectedRows
+        };
+
+        return created_resource;
+    }
+
+    update = async(columns, id) => {
+        const { columnSet, values } = multipleColumnSet(columns);
+
+        const sql = `UPDATE ${tables.PostUploads} 
+        SET ${columnSet} 
+        WHERE resource_id = ?`;
+
+        const result = await DBService.query(sql, [...values, id]);
+
+        return result;
+    }
+
+    delete = async(id) => {
+        const sql = `DELETE FROM ${tables.PostUploads}
+        WHERE resource_id = ?`;
+
+        const result = await DBService.query(sql, [id]);
+        const affectedRows = result ? result.affectedRows : 0;
+
+        return affectedRows;
+    }
+}
+
+module.exports = new PostUploadModel;
