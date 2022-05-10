e5f8b98b30d6099a8cffb22dbc383dd059e0b937
arafaysaleem <a.rafaysaleem@gmail.com>
2022-05-11 02:37:19 +0500

    refact(Loaders): limit sentry to production environment

--- src/loaders/express.loader.js
+++ src/loaders/express.loader.js
@@ -2,20 +2,23 @@ const express = require("express");
 const cors = require("cors");
 const Sentry = require("@sentry/node");
 const { SentryLoader } = require("./sentry.loader");
+const { Config } = require("../configs/config");
 
 class ExpressLoader {
     static init() {
         const app = express();
 
-        // init Sentry SDK for error logging
-        SentryLoader.init(app);
+        if (Config.NODE_ENV === 'production') {
+            // init Sentry SDK for error logging
+            SentryLoader.init(app);
 
-        // To monitor release health of each request in session aggregates mode
-        // This must be the first middleware
-        app.use(Sentry.Handlers.requestHandler());
+            // To monitor release health of each request in session aggregates mode
+            // This must be the first middleware
+            app.use(Sentry.Handlers.requestHandler());
 
-        // TracingHandler creates a trace for every incoming request
-        app.use(Sentry.Handlers.tracingHandler());
+            // TracingHandler creates a trace for every incoming request
+            app.use(Sentry.Handlers.tracingHandler());
+        }
 
         // Middleware that transforms the raw string of req.body into json
         app.use(express.json());
--- src/loaders/middleware.loader.js
+++ src/loaders/middleware.loader.js
@@ -1,6 +1,7 @@
 const { InvalidEndpointException } = require('../utils/exceptions/api.exception');
 const errorMiddleware = require('../middleware/error.middleware');
 const Sentry = require("@sentry/node");
+const { Config } = require('../configs/config');
 
 class MiddlewareLoader {
     static init(app) {
@@ -10,9 +11,11 @@ class MiddlewareLoader {
             next(err);
         });
 
-        // Sentry error loggin middleware
-        // This must be after routes loader and before error middleware
-        app.use(Sentry.Handlers.errorHandler());
+        if (Config.NODE_ENV === 'production') {
+            // Sentry error loggin middleware
+            // This must be after routes loader and before error middleware
+            app.use(Sentry.Handlers.errorHandler());
+        }
 
         // Error middleware
         // This must be the last middleware
